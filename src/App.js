b5a082a666dc7a80b333a2691b541399854dae1d
rohitkolli19 <knsvrohit@gmail.com>
2023-06-12 11:53:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
+}
+
+export default App;
+
